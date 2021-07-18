b44637973d827203b87667a22f92f4cf3eda86c0
Krzysztof Kowalczyk <kowalczykkrzysztof893@gmail.com>
2021-07-18 23:51:53 +0200

    Fixed axios test

--- frontend/src/features/words/fetchWord.test.ts
+++ frontend/src/features/words/fetchWord.test.ts
@@ -8,11 +8,13 @@ const mockedAxios = axios as jest.Mocked<typeof axios>;
 
 describe('fetchData', () => {
   test('fetches new word from my API', async () => {
-    const data: WordProps = {
-      word: '勝負',
-      wordExists: true,
-      reading: 'しょうぶ',
-      definitions: ['victory or defeat'],
+    const data: { data: WordProps } = {
+      data: {
+        word: '勝負',
+        wordExists: true,
+        reading: 'しょうぶ',
+        definitions: ['victory or defeat'],
+      },
     };
     mockedAxios.get.mockImplementationOnce(() => Promise.resolve(data));
     await expect(fetchWord()).resolves.toEqual(data);
